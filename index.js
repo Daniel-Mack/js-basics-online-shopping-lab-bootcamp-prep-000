1b148f2dbfb89dace27f90606aebcf54c4ac2a64
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-26 18:17:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,35 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+  function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min
+  }
+  let price = getRandomInt(0, 100)
+  let cartItem = {itemName: item, itemPrice: price}
+  cart.push(cartItem) 
+  return `${item} has been added to your cart.`
+} 
+
+
 
 function viewCart() {
-  // write your code here
-}
 
+   for ( let i = 0; i < cart.length; i++) {
+     if (i === 0) {
+       return  "Your shopping cart is empty."
+     } else if (i === 1) {
+       return `In your cart, you have ${item} at ${price}.`
+     } else if (i === 2) {
+       return `In your cart, you have ${item} at ${price}.`
+     } else if (i === 3) {
+       return `In your cart, you have ${item} at ${price}.`
+     }
+  }
+  getcart()
+}
+ 
 function total() {
   // write your code here
 }
